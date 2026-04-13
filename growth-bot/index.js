f19f2c21ec42d337e20745c14aada139d8caf716
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-13 03:19:48 +0300

    ci: add growth pulse workflow for SDK downloads

---
+++ growth-bot/index.js
@@ -0,0 +1,5 @@
+import * as sdk from 'dice-roll-sdk';
+
+console.log('Growth bot running for dice-roll-sdk...');
+console.log('SDK exports:', Object.keys(sdk).join(', '));
+console.log('Smoke check passed.');
