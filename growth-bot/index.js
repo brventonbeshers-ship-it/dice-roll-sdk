89e91423c6dce9b1b0faffd765289bc9137c2062
brventonbeshers-ship-it <brventonbeshers@gmail.com>
2026-04-17 13:18:30 +0300

    chore: clean up index.js

--- growth-bot/index.js
+++
@@ -1,5 +0,0 @@
-import * as sdk from 'dice-roll-sdk';
-
-console.log('Growth bot running for dice-roll-sdk...');
-console.log('SDK exports:', Object.keys(sdk).join(', '));
-console.log('Smoke check passed.');
